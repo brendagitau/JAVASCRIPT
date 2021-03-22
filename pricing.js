53047760dec2a98acc479a25b315044e9a57eac2
brendagitau <79209524+brendagitau@users.noreply.github.com>
2021-03-22 09:32:36 +0300

    Add files via upload
    slides h/w and method example

---
+++ pricing.js
@@ -0,0 +1,79 @@
+/*Write a program to calculate purchases from a kiosk. For e.g. given the fruit name 'orange' 
+the program should fetch its price (30.00) and multiply with the quantity requested (2) and 
+return total cost in this printed format (2 Oranges for KES 60.00). 
+
+calculateFruitCost(fruitName, quantity)
+
+For example, calculateFruitCost('orange', 2) should return "2 Oranges for KES 60.00"*/
+
+/*function kiosk(fruitName,price,quantity){
+    var totalCost =price * quantity
+    var totalAmount =quantity+" "+fruitName+"  for KES  "+totalCost
+        console.log(totalAmount);
+       
+       
+        function calcFruitCost(fruitName,quantity
+            ){
+            var fruitCost=quantity*price
+            var fruitCost=quantity+" "+fruitName+" "+"for KES"+ " "+fruitCost
+            console.log(fruitCost)
+        }
+        calcFruitCost("avocado",70)
+    }
+    
+ kiosk("avocado",50,10) ;  
+// examples of 
+
+  
+   let door={
+     type:"steel",
+     colour:"green"
+     shape:"rectangular",
+      
+   door.buy=function(){
+     alert("durable");
+   }
+    door.buy();
+   
+   function door(){
+     var type="steel"
+     var colour="green"
+     var shape="rectangular"
+     function buy(){
+       var conclusion="colour+ " " +steel+" "+shape+ " "door",
+     }
+console.log(conclusion);
+   }
+   buy()
+   
+   */
+
+   //object &properties
+  var cake={
+      name:"party",
+      size: 20,
+      type:30,
+     
+
+
+  }
+  //method&function&properties
+    console.log(cake);
+
+    
+var Cake/*object */={
+ 
+    /*property*/ 
+
+    cakeName:"shereheCakes",
+    quantity:10,
+    price:2000,
+    cakePrice: function (quantity,price){
+      cakePrice=quantity*price;
+        
+        cakePrice =price;}
+
+    
+}
+Cake.cakePrice(10,2000);
+console.log(" sherehe cakes " + cake.quantity + " costs "  +  cakePrice);
